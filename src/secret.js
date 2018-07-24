d7d4cb59a6db1efaa3cd5473958e1b3a2d093e80
Andrea Thniah <andreathniah@gmail.com>
2018-07-24 17:05:41 +0800

    for heroku deployment

--- src/secret.js
+++ src/secret.js
@@ -1,10 +1,10 @@
-const proxyURL = process.env.PROXY_URL;
-const omdKey = process.env.OMD_KEY; // https://www.omdbapi.com
-const mdbKey = process.env.MDB_KEY; // https://www.themoviedb.org
+const proxyURL = process.env.REACT_APP_PROXY_URL;
+const omdKey = process.env.REACT_APP_OMD_KEY; // https://www.omdbapi.com
+const mdbKey = process.env.REACT_APP_MDB_KEY; // https://www.themoviedb.org
 const firebaseKey = {
-	apiKey: process.env.FIREBASE_KEY,
-	authDomain: process.env.FIREBASE_DOMAIN,
-	databaseURL: process.env.FIREBASE_URL
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_URL
 };
 
 export { omdKey }; // named export
