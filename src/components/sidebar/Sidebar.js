3b827e7b28177a0c580a51f1214b33f3434a2c8a
Andrea Thniah <andreathniah@gmail.com>
2018-08-24 17:00:41 +0800

    changing class in className

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,63 +5,73 @@ import "./Sidebar.css";
 import logo from "../../images/logo.jpg";
 
 class Sidebar extends React.Component {
-	render() {
-		const { roombox, match, removeFromBox, toggle } = this.props;
-		const { roomId } = match.params;
+  render() {
+    const { roombox, match, removeFromBox, toggle } = this.props;
+    const { roomId } = match.params;
 
-		const item = Object.keys(roombox).map(id => {
-			return (
-				<SidebarItem
-					key={id}
-					index={id}
-					details={roombox[id]}
-					removeFromBox={removeFromBox}
-					toggle={toggle}
-				/>
-			);
-		});
+    const item = Object.keys(roombox).map(id => {
+      return (
+        <SidebarItem
+          key={id}
+          index={id}
+          details={roombox[id]}
+          removeFromBox={removeFromBox}
+          toggle={toggle}
+        />
+      );
+    });
 
-		return (
-			<div className = "SideBar">
-				<div>
-					<img src={logo} className = "logo"/>
-				</div>
-				
-				<div className = "menu">
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
-					</button>
+    return (
+      <div className="SideBar">
+        <div>
+          <img src={logo} className="logo" />
+        </div>
 
-					<div class="collapse navbar-collapse" id="navbarsExample02">
-						<ul class="navbar-nav mr-auto subMenu">
-							<li class="nav-item">
-								<Link to={`/${roomId}/leaderboard`}>LEADERBOARD</Link>
-							</li>
-							<li class="nav-item">
-								<Link to={`/${roomId}/movies`}>MOVIE DETAILS</Link>
-							</li>
-							<li class="nav-item">
-								<Link to={`/${roomId}/polls`}>POLLS</Link>
-								{item}
-							</li>
-						</ul>
-					</div>			
-				</div>				
-				
-				<div className = "SideBarContent">
-					<Link to={`/${roomId}/leaderboard`}>LEADERBOARD</Link>
-				</div>
-				<div className = "SideBarContent">
-					<Link to={`/${roomId}/movies`}>MOVIE DETAILS</Link>
-				</div>
-				<div className = "SideBarContent">
-					<Link to={`/${roomId}/polls`}>POLLS</Link>
-					{item}
-				</div>
-				<br />
-			</div>		
-		);
-	}
+        <div className="menu">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarsExample02"
+            aria-controls="navbarsExample02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon">
+              <i className="fas fa-bars" />
+            </span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExample02">
+            <ul className="navbar-nav mr-auto subMenu">
+              <li className="nav-item">
+                <Link to={`/${roomId}/leaderboard`}>LEADERBOARD</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={`/${roomId}/movies`}>MOVIE DETAILS</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={`/${roomId}/polls`}>POLLS</Link>
+                {item}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="SideBarContent">
+          <Link to={`/${roomId}/leaderboard`}>LEADERBOARD</Link>
+        </div>
+        <div className="SideBarContent">
+          <Link to={`/${roomId}/movies`}>MOVIE DETAILS</Link>
+        </div>
+        <div className="SideBarContent">
+          <Link to={`/${roomId}/polls`}>POLLS</Link>
+          {item}
+        </div>
+        <br />
+      </div>
+    );
+  }
 }
 
 export default Sidebar;
--- src/components/sidebar/SidebarItem.js
+++ src/components/sidebar/SidebarItem.js
@@ -2,23 +2,23 @@ import React from "react";
 import "./SidebarItem.css";
 
 class SidebarItem extends React.Component {
-	// remove title from roombox database onclick
-	onClickRemove = () => {
-		const { index } = this.props;
-		this.props.removeFromBox(index);
-		this.props.toggle(index);
-	};
+  // remove title from roombox database onclick
+  onClickRemove = () => {
+    const { index } = this.props;
+    this.props.removeFromBox(index);
+    this.props.toggle(index);
+  };
 
-	render() {
-		return (
-			<li>
-				{this.props.details.Title}
-				<button onClick={this.onClickRemove} className="minus">
-					<i className="fas fa-minus minusSign" />
-				</button>
-			</li>
-		);
-	}
+  render() {
+    return (
+      <li>
+        {this.props.details.Title}
+        <button onClick={this.onClickRemove} className="minus">
+          <i className="fas fa-minus minusSign" />
+        </button>
+      </li>
+    );
+  }
 }
 
 export default SidebarItem;
